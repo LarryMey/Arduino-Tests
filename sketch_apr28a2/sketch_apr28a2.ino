610217dab855b54b5cdd89f76615bbc29d305818
LarryMey <mey@gmx.net>
2014-04-28 19:05:34 +0200

    Poti

---
+++ sketch_apr28a2/sketch_apr28a2.ino
@@ -0,0 +1,19 @@
+int PotiPin = A0;
+
+int PinLED = 11;
+
+int PotiWert;
+
+
+void setup(){
+  pinMode(PotiPin, INPUT); //brauch man eigentlich nicht hin schreiben
+    
+  pinMode(PinLED, OUTPUT);
+}
+
+
+void loop(){
+  PotiWert = analogRead(PotiPin);
+  analogWrite(PinLED, PotiWert);
+    
+}
