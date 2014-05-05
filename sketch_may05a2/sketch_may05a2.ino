86394b290805cfb8416c9ec687af951b53f020e2
LarryMey <mey@gmx.net>
2014-05-05 18:13:42 +0200

    Photo-Widerstand statt Poti
    gleiche Programmierung

---
+++ sketch_may05a2/sketch_may05a2.ino
@@ -0,0 +1,33 @@
+int PotiPin = A0;
+
+int PinLED = 9;
+
+int PotiWert;  
+     // den wollen wir einlesen
+     // Poti ersetzt durch Photo-Widerstand 
+     // In Abhängigkeit der Helligkeit werden verschiedene Werte eingelesen
+
+
+void setup(){
+  Serial.begin(19200); 
+    // Datenübertragungsrate pro sec. (baut Zahlen)
+  Serial.print("Setup Ende"); 
+    //in Anführungszeichen ist Text der vom Computer in Askiwerte umwandelt und verschickt anstatt die Worte als Aufforderung zu verstehen
+  pinMode (PotiPin, INPUT);  
+  pinMode(PinLED, OUTPUT);
+}
+
+
+void loop(){
+  PotiWert = analogRead(PotiPin); 
+    //einlesen des Potis (analogRead(PotiPin)) wird gespeichert in PotiWert (= ist ein zuweisen)
+  Serial.println(PotiWert); 
+    //Ausgeben der Variablen auf dem seriellen Port 
+    //Serial.print schreibt alles in eine Zeile 
+    //Serial.println macht Zeilenumbrüche nach jedem Wert
+  analogWrite(PinLED, PotiWert / 4); 
+    //führt die Pulsweitenmodulation auf der LED aus über den Bereich des PotiWerts/4
+  delay(100); 
+    //10 mal pro sec
+    
+}
